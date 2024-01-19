834fe4169587a7536da8e45ebd8db0720e096c61
Landry Christensen <80078355+lchristensen6@users.noreply.github.com>
2024-01-18 18:49:08 -0600

    Added values in Constants to enable driving with the button bindings and settings from last years drive system.

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -14,9 +14,12 @@ import com.pathplanner.lib.util.ReplanningConfig;
 
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.geometry.Translation3d;
 import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
 import edu.wpi.first.math.util.Units;
+import swervelib.math.Matter;
+import swervelib.parser.PIDFConfig;
 
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide numerical
@@ -31,7 +34,35 @@ import edu.wpi.first.math.util.Units;
 public final class Constants
 {
     
+    public static final double ROBOT_MASS = (148 - 20.3) * 0.453592; // 32lbs * kg per pound
+    public static final Matter CHASSIS    = new Matter(new Translation3d(0, 0, Units.inchesToMeters(8)), ROBOT_MASS);
+    public static final double LOOP_TIME  = 0.13; //s, 20ms + 110ms sprk max velocity lag
 
+    public static final class Auton
+    {
+
+        public static final PIDFConfig TranslationPID     = new PIDFConfig(0.7, 0, 0);
+        public static final PIDFConfig angleAutoPID = new PIDFConfig(0.4, 0, 0.01);
+
+        public static final double MAX_ACCELERATION = 2;
+    }
+
+    public static final class Drivebase
+    {
+
+        // Hold time on motor brakes when disabled
+        public static final double WHEEL_LOCK_TIME = 10; // seconds
+    }
+
+    public static class OperatorConstants
+    {
+
+        // Joystick Deadband
+        public static final double LEFT_X_DEADBAND = 0.01;
+        public static final double LEFT_Y_DEADBAND = 0.01;
+        public static final double RIGHT_X_DEADBAND = 0.01;
+        public static final double TURN_CONSTANT = 0.75;
+    }
     /** Constants that define the drivetrain as a whole */
     public static final class DriveConstants
     {
