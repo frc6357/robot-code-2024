c2413a213997fbaacbb8cd4ff35f3490e506c256
Landry Christensen <80078355+lchristensen6@users.noreply.github.com>
2024-02-22 16:51:50 -0600

    Inverted motor and change PID to positive

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -62,8 +62,8 @@ public final class Constants
         // The steer motor uses any SwerveModule.SteerRequestType control request with the
         // output type specified by SwerveModuleConstants.SteerMotorClosedLoopOutput
         private static final Slot0Configs steerGains = new Slot0Configs()
-        .withKP(-100).withKI(0).withKD(-0.2)
-        .withKS(0).withKV(-1.5).withKA(0);
+        .withKP(100).withKI(0).withKD(0.2)
+        .withKS(0).withKV(1.5).withKA(0);
         // When using closed-loop control, the drive motor uses the control
         // output type specified by SwerveModuleConstants.DriveMotorClosedLoopOutput
         private static final Slot0Configs driveGains = new Slot0Configs()
@@ -93,7 +93,7 @@ public final class Constants
         private static final double kSteerGearRatio = 21.428571428571427;
         private static final double kWheelRadiusInches = 2;
 
-        private static final boolean kSteerMotorReversed = false;
+        private static final boolean kSteerMotorReversed = true;
         private static final boolean kInvertLeftSide = false;
         private static final boolean kInvertRightSide = true;
 
