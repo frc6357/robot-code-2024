0633856994756699dd13521aea9479651ebbee09
Alexa <116688112+alexarayship@users.noreply.github.com>
2024-02-22 10:21:32 -0600

    Feature/intake (#24)
    * Intake Subsystem
    
    Created the intake subsystem, intake file binder, intake command, robot container, json.
    
    * Adjusted Intake subsystem to use two motors
    
    * Added bottom intake motor as a follower of the top intake motor
    
    ---------
    
    Co-authored-by: ERobin07 <156146464+ERobin07@users.noreply.github.com>
    Co-authored-by: Landry Christensen <80078355+lchristensen6@users.noreply.github.com>

--- src/main/java/frc/robot/Ports.java
+++ src/main/java/frc/robot/Ports.java
@@ -147,9 +147,8 @@ public class Ports
     public static class intakePorts 
     {
         private static final String busName = "";
-
-        public static final CANPort kTopMotor = new CANPort(50, busName);
-        public static final CANPort kBottomMotor = new CANPort(51, busName);
+        public static final CANPort kTopIntakeMotor = new CANPort(50, busName);
+        public static final CANPort kBottomIntakeMotor = new CANPort(51, busName);
         
     }
 }
--- src/main/java/frc/robot/commands/IntakeCommand.java
+++ src/main/java/frc/robot/commands/IntakeCommand.java
@@ -3,6 +3,7 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.SK24Intake;
 
+
 public class IntakeCommand extends Command
 {
   @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
