d1bcb098735c25834678f1b8906cb401f0c1645f
Landry Christensen <80078355+lchristensen6@users.noreply.github.com>
2024-02-26 14:16:37 -0600

    Added binding for party mode that runs an animation

--- src/main/java/frc/robot/bindings/SK24LightBinder.java
+++ src/main/java/frc/robot/bindings/SK24LightBinder.java
@@ -4,6 +4,8 @@ import static frc.robot.Ports.OperatorPorts.kPartyMode;
 
 import java.util.Optional;
 
+import com.ctre.phoenix.led.ColorFlowAnimation.Direction;
+
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.subsystems.SK24Light;
@@ -20,11 +22,12 @@ public class SK24LightBinder implements CommandBinder{
     public void bindButtons()
     {
         // If subsystem is present then this method will bind the buttons
+        Direction direction = Direction.Forward;
         if (subsystem.isPresent())
         {
             SK24Light m_light = subsystem.get();
             
-            lightButton.onTrue(new InstantCommand(() -> m_light.setOrange(1000)));
+            lightButton.onTrue(new InstantCommand(() -> m_light.FlowAnimate(3, 168, 181, 0.5, 64, direction,8)));
         }
     }
 }
