50ab93232c051d104f6f7d16ec1d676b82abcc56
Landry Christensen <80078355+lchristensen6@users.noreply.github.com>
2024-03-02 16:11:30 -0600

    Removed unneeded position commands

--- src/main/java/frc/robot/OnTheFly.java
+++ src/main/java/frc/robot/OnTheFly.java
@@ -10,9 +10,6 @@ import edu.wpi.first.wpilibj2.command.Command;
 public final class OnTheFly {
     // Load the path we want to pathfind to and follow
     public static final PathPlannerPath scoreAmp = PathPlannerPath.fromPathFile("AmpScore");
-    public static final PathPlannerPath scorePos1 = PathPlannerPath.fromPathFile("ScorePos1");
-    public static final PathPlannerPath scorePos2 = PathPlannerPath.fromPathFile("ScorePos2");
-    public static final PathPlannerPath scorePos3 = PathPlannerPath.fromPathFile("ScorePos3");
     public static final PathPlannerPath centerStage = PathPlannerPath.fromPathFile("StageCenter");
     public static final PathPlannerPath leftStage = PathPlannerPath.fromPathFile("StageLeft");
     public static final PathPlannerPath rightStage = PathPlannerPath.fromPathFile("StageRight");
@@ -24,21 +21,6 @@ public final class OnTheFly {
 
     public static final Command scoreAmpCommand = AutoBuilder.pathfindThenFollowPath(
             scoreAmp,
-            constraints,
-            0.0 // Rotation delay distance in meters. This is how far the robot should travel before attempting to rotate.
-    );
-    public static final Command scorePos1Command = AutoBuilder.pathfindThenFollowPath(
-            scorePos1,
-            constraints,
-            0.0 // Rotation delay distance in meters. This is how far the robot should travel before attempting to rotate.
-    );
-    public static final Command scorePos2Command = AutoBuilder.pathfindThenFollowPath(
-            scorePos2,
-            constraints,
-            0.0 // Rotation delay distance in meters. This is how far the robot should travel before attempting to rotate.
-    );
-    public static final Command scorePos3Command = AutoBuilder.pathfindThenFollowPath(
-            scorePos3,
             constraints,
             0.0 // Rotation delay distance in meters. This is how far the robot should travel before attempting to rotate.
     );
