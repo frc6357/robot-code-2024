bb4f1403709d3cd4f0462a004db97cb6931f62a3
ERobin07 <156146464+ERobin07@users.noreply.github.com>
2024-03-09 07:46:21 -0600

    Added fixed auto

---
+++ src/main/java/frc/robot/commands/commandGroups/Pos1Fixed.java
@@ -0,0 +1,34 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands.commandGroups;
+
+import static frc.robot.Constants.LauncherAngleConstants.kPos1Angle;
+import static frc.robot.Constants.LauncherConstants.kLauncherLeftSpeed;
+import static frc.robot.Constants.LauncherConstants.kLauncherRightSpeed;
+
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
+import frc.robot.commands.AngleCommandAuto;
+import frc.robot.commands.LaunchCommandAuto;
+import frc.robot.subsystems.SK24Launcher;
+import frc.robot.subsystems.SK24LauncherAngle;
+
+public class Pos1Fixed extends ParallelCommandGroup {
+   
+    /**
+     * Shoot note into speaker from starting position 1
+     * @param launcher Launcher subsystem to use
+     * @param arm Launcher angle subsystem to use
+     */
+    public Pos1Fixed(SK24Launcher launcher, SK24LauncherAngle arm)
+    {
+        addCommands(
+            new AngleCommandAuto(kPos1Angle, arm),
+            new LaunchCommandAuto(kLauncherLeftSpeed, kLauncherRightSpeed, launcher),
+            new WaitCommand(0.25)
+        );
+    }
+
+}
