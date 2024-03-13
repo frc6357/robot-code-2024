39c5ff0a7654b31b114de82b25e73aa4fab10bb5
Landry Christensen <80078355+lchristensen6@users.noreply.github.com>
2024-03-12 19:05:13 -0500

    Cleaning commands that we don't need / updating

--- src/main/java/frc/robot/commands/commandGroups/IntakeTransferCommandGroup.java
+++ src/main/java/frc/robot/commands/commandGroups/IntakeTransferCommandGroup.java
@@ -5,6 +5,8 @@
 package frc.robot.commands.commandGroups;
 
 import static frc.robot.Constants.IntakeConstants.kIntakeSeconds;
+import static frc.robot.Constants.IntakeConstants.kIntakeSpeed;
+import static frc.robot.Constants.LauncherConstants.kTransferSpeed;
 
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.ParallelRaceGroup;
@@ -19,16 +21,31 @@ public class IntakeTransferCommandGroup extends SequentialCommandGroup {
     
     public IntakeTransferCommandGroup(SK24Launcher launcher, SK24Intake intake)
     {
+        // addCommands(
+        //     new ParallelRaceGroup(
+        //         new IntakeTransferCommand(intake, launcher),
+        //         new WaitCommand(kIntakeSeconds)
+        //     ),
+        //     new WaitCommand(0.5),
+        //     new LaunchCommand(-0.05, -0.05, launcher),
+        //     new WaitCommand(0.5),
+        //     new InstantCommand(() -> intake.stopIntake()),
+        //     new InstantCommand(() -> launcher.stopTransfer()),
+            
+        //     new InstantCommand(() -> launcher.stopLauncher())
+            
+        // );
+
         addCommands(
             new ParallelRaceGroup(
-                new IntakeTransferCommand(intake, launcher),
-                new LaunchCommand(-0.05, -0.05, launcher),
+                new IntakeTransferCommand(kIntakeSpeed, kTransferSpeed, intake, launcher),
                 new WaitCommand(kIntakeSeconds)
             ),
-            new InstantCommand(() -> intake.stopIntake()),
-            new InstantCommand(() -> launcher.stopTransfer()),
-            
-            new InstantCommand(() -> launcher.stopLauncher())
+            new WaitCommand(0.5),
+            new ParallelRaceGroup(
+                new IntakeTransferCommand(-0.1, -0.1, intake, launcher),
+                new WaitCommand(3.0)
+            )
             
         );
     }
