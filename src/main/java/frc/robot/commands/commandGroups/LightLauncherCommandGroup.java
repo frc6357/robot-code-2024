d91124ece9f53eb5de0a9543dcc554f64ffafd01
Landry Christensen <80078355+lchristensen6@users.noreply.github.com>
2024-04-03 16:02:15 -0500

    Create LightLauncherCommandGroup.java

---
+++ src/main/java/frc/robot/commands/commandGroups/LightLauncherCommandGroup.java
@@ -0,0 +1,29 @@
+package frc.robot.commands.commandGroups;
+
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
+
+import static frc.robot.Constants.LauncherConstants.kSpeakerDefaultLeftSpeed;
+import static frc.robot.Constants.LauncherConstants.kSpeakerDefaultLeftSpeedKey;
+import static frc.robot.Constants.LauncherConstants.kSpeakerDefaultRightSpeed;
+import static frc.robot.Constants.LauncherConstants.kSpeakerDefaultRightSpeedKey;
+import static frc.robot.Constants.LauncherConstants.speakerSpeedLeft;
+import static frc.robot.Constants.LauncherConstants.speakerSpeedRight;
+
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import frc.robot.preferences.Pref;
+import frc.robot.preferences.SKPreferences;
+import frc.robot.utils.SKCANLight;
+
+public class LaunchSpeakerCommandGroup extends SequentialCommandGroup{
+
+    public LaunchSpeakerCommandGroup(SKCANLight light)
+    {
+        addCommands(
+            new InstantCommand(() -> light.setRed()),
+            new WaitCommand(1.0),
+            new InstantCommand(() -> light.setGreen())
+        );
+    }
+    
+}
